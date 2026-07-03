7f06652c5bdbb0efb82517877ec281dcab4befca
Jbosco339 <benjaminadakole20@gmail.com>
2026-07-03 14:04:29 +0100

    view more button bug fixed

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
                 href={project.link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="absolute inset-0 md:hidden flex items-center justify-center text-center px-4"
+                className="absolute inset-0 z-10 md:hidden flex items-center justify-center text-center px-4"
               >
                 <span className="bg-gray-400/10 text-black text-sm font-medium px-4 py-2 rounded-full backdrop-blur-sm shadow-sm">
                   View Project →
@@ -39,7 +39,7 @@ const Projects = () => {
               </a>
 
               {/* Overlay */}
-              <div className="absolute inset-0 bg-black/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col items-center justify-center text-center p-8">
+              <div className="absolute inset-0 bg-black/80 opacity-0 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto transition-all duration-300 flex flex-col items-center justify-center text-center p-8">
                 <h3 className="text-3xl font-bold text-white">
                   {project.title}
                 </h3>
